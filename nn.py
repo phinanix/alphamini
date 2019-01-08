524856540f79aec665a20df4ed6c7635fa3bdbfd
Seraphina Nix <seraphina.nix@gmail.com>
2019-01-08 15:47:14 -0500

    added docstrings

--- nn.py
+++ nn.py
@@ -12,12 +12,20 @@ class Network():
     and a scalar value output in (-1,1)
     '''
 
+    '''initializes network to support inputs of sizexsizex(2*histsize+1)
+    and performs any other initialization'''
     def __init__(self, size, hist_size):
         #maybe take a number of layers and a network type or something?
         self.size=size
         self.hist_size = hist_size
         self.network = None
-        
+
+    '''takes as input a board of sizexsizex(2*hist_size)
+    returns a tuple p,v with p an array of sizexsize that sums to 1 
+    of probabilities of selecting moves
+    v a scalar in (-1, 1) representing the likelihood of winning from the 
+    current player's persepctive
+    '''
     def evaluate(self, board, player):
         player_plane = np.full( (self.size,self.size), player)
         input_block = np.empty( (self.size,self.size,(self.hist_size*2)+1) )
